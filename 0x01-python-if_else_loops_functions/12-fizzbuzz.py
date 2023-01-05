508d110c472ef243da89c7c51a7769b5423e3686
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-05 02:32:15 +0000

    Fixed wrong output issue

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,11 +9,13 @@ def fizzbuzz():
     For multiples of three and five, print FizzBuzz instead of the number.
     """
     for i in range(1, 101):
-        if i % 3 == 0:
-            print("Fizz", end=' ')
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
         elif i % 5 == 0:
             print("Buzz", end=' ')
-        elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=' ')
+        elif i % 3 == 0:
+            print("Fizz", end=' ')
         else:
-            print("{}".format(), end=' ')
+            print("{}".format(i), end=' ')
+
+fizzbuzz()
--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -5,6 +5,5 @@ def print_last_digit(number):
     function that prints the last digit of a number
     """
     ls_digit = int(abs(number) % 10)
-    if number < 0:
-        ls_digit = ls_digit * -1
-    print("{}".format(ls_digit))
+    print("{}".format(ls_digit), end='')
+    return ls_digit
