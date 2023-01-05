2025c83c50ef00156b031348ba574456490085eb
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-05 02:11:26 +0000

    Fixed ls_dgt spelling issue

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,7 +6,7 @@ msg = ["Last digit of", "is"]
 con_msg = ["and is greater than 5", "and is 0", "and is less than 6 and not 0"]
 ls_dgt = int(abs(number) % 10)
 if number < 0:
-    ls_dgt = ls_digit * -1
+    ls_dgt = ls_dgt * -1
 
 # check for given conditions
 if ls_dgt > 5:
