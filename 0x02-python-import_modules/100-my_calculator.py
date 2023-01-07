9d3d5c6b9ba25f75dd2645df6045a220f36363bf
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-07 13:00:01 +0000

    Modified to be less verbose and work for division

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,53 +1,22 @@
 #!/usr/bin/python3
 # Author - Lawal Emmanuel
 
-# run only as script/ not when imported
-if __name__ == '__main__':
-    # import module(s)
+if __name__ == "__main__":
+    import sys
+    sys.path.append("./test_files")
+    """Handle basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div
+    import sys
 
-    # init variables
-    err_msg_arg = "Usage: ./100-my_calculator.py <a> <operator> <b>"
-    err_msg_opr = "Unknown operator. Available operators: +, -, * and /"
-    sysa = sys.argv
-    sysex = sys.exit
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    # declare function
-    def is_operator(op):
-        """ check if argument is maths operator """
-        ret = False
-        opr_lst = ['+', '-', '*', '/']
-        if op in opr_lst:
-            ret = True
-        return ret
+    ops = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(ops.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-    # define function
-    def set_operator(op):
-        """ sets the operator to be used """
-        if op == '+':
-            op = add
-        elif op == '-':
-            op = sub
-        elif op == '*':
-            op == mul
-        else:
-            op == div
-        return op
-
-    # checks for correct number of arguments
-    if len(sysa)-1 != 3:
-        print("{}".format(err_msg_arg))
-        sysex(1)
-
-    # checks for correct math operator
-    opr = sysa[2]
-    if not is_operator(opr):
-        print("{}".format(err_msg_opr))
-        sysex(1)
-
-    # performs requested operation
-    opr = set_operator(opr)
-    a = int(sysa[1])
-    b = int(sysa[3])
-    opr_sign = sysa[2]
-    print("{} {} {} = {}".format(a, opr_sign, b, opr(a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
