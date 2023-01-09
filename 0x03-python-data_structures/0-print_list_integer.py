d85e543b3eff8aa02229115c0bc958a6462d0731
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-09 11:42:30 +0000

    Removed author tag and func docstring

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# Author - Lawal Emmanuel
-
 def print_list_integer(my_list=[]):
-    """ This function that prints all integers of a list """
     for i in range(len(my_list)):
         print("{}".format(my_list[i]))
