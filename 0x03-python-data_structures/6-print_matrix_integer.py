d6613483eb6930f8aacfa461abb741eca907f699
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-10 06:34:04 +0000

    Fixed the print format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,5 +5,5 @@ def print_matrix_integer(matrix=[[]]):
     """ This function that prints a matrix of integers """
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            print("{:d}".format(matrix[i][j]), end=" ")
+            print("{:d}".format(matrix[i][j]), end="" if j+1 == len(matrix[0]) else " ")
         print()
