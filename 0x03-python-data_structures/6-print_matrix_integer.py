893c86ce5af34fc3ab2b0f3649dcd5eb2fdf7d0c
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-10 22:09:27 +0000

    Removed test code

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,14 +7,3 @@ def print_matrix_integer(matrix=[[]]):
         for j in i:
             print("{:d}".format(j), end=" " if j != i[-1] else "")
         print()
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
