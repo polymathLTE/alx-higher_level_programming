4169377b8781d5ee47f23faad0f16627b183cbaf
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-10 22:05:47 +0000

    Fixed the line length and changed loop format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,18 @@
 
 def print_matrix_integer(matrix=[[]]):
     """ This function that prints a matrix of integers """
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            print("{:d}".format(matrix[i][j]), end="" if j+1 == len(matrix[0]) else " ")
+    for i in matrix:
+        for j in i:
+            print("{:d}".format(j), end="" if j == i[-1] else " ")
         print()
+
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print("--")
+print_matrix_integer()
