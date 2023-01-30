5dd92572065fea40a27c270577288bcc5d1ab974
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-30 04:08:20 +0000

    Added conditions to test the set position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,10 +44,11 @@ class Square:
 
     @position.setter
     def position(self, value):
-        """method to set the position of Square """
-        if not isinstance(value, tuple) or len(value) != 2:
-            raise TypeError("position must be a tuple of 2 positive integers")
-        elif value[0] <= 0 or value[1] <= 0:
+        """ method to set the position of Square """
+        if (not isinstance(value, tuple) or
+                len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self._position = value
 
