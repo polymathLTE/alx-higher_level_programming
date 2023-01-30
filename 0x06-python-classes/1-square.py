4e3af1d4a1d0670041de87fc29a4ebdb8fc35677
polymathLTE <lawaltemmanuel@gmail.com>
2023-01-30 01:46:59 +0000

    Removed conditional statement

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -8,5 +8,4 @@ class Square:
     """ defines a square by attributes """
     def __init__(self, size):
         """ this is instatiation code """
-        if size is not None:
-            self._size = size
+        self._size = size
