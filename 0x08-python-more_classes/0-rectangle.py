ec9df486d96001351243491ce70072f3efbf69bf
polymathLTE <lawaltemmanuel@gmail.com>
2023-02-02 07:19:00 +0000

    Added task file 3 - 7

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,5 +2,7 @@
 # Author - Lawal Emmanuel
 
 """ This defines a rectangle class """
+
+
 class Rectangle:
     """ simple implementation of a rectangle """
---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+# Author - Lawal Emmanuel
+
+""" This defines a rectangle class """
+
+
+class Rectangle:
+    """ simple implementation of a rectangle """
+    def __init__(self, width=0, height=0):
+        """this initializes an object on creation
+        Args:
+            width (int): the init width of rectangle obj
+            height (int): the init height of rectangle obj
+        """
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """retrieves the width of the rectangle obj"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """sets the width if the rectangle obj
+        Args:
+            value (int): the setter value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """retrieves the height of the rectangle obj"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """sets the height if the rectangle obj
+        Args:
+            value (int): the setter value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        """calc. and returns the area of the rec. obj
+        Returns:
+            The area of the rect. obj.
+        """
+        return (self._width * self._height)
+
+    def perimeter(self):
+        """calc. and return the perimeter of the rec. obj
+        Returns:
+            the perimeter of the rect. obj
+        """
+        if self._width == 0 or self.height == 0:
+            return 0
+        return 2 * (self._width + self._height)
+
+    def __str__(self):
+        """computes and displays the size of the rect. obj using
+        Returns:
+            displays a size representation of rect obj using '#
+        """
+        wid = self._width
+        hgt = self._height
+        if wid == 0 or hgt == 0:
+            return ""
+        for i in range(hgt):
+            [print('#', end='') for j in range(wid)]
+            print()
+        return ""
+
+    def __repr__(self):
+        """returns a canonical representation of the rect. obj;
+        a curated string that can be used by eval
+        Returns:
+            curated f-string
+        """
+        return ("Rectangle({}, {})".format(self._width, self._height))
