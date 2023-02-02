5237aa21f2548d6698993d4c037986bc61325c7d
polymathLTE <lawaltemmanuel@gmail.com>
2023-02-02 22:14:15 +0000

    Fixed the init method

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -12,8 +12,8 @@ class Rectangle:
             width (int): the init width of rectangle obj
             height (int): the init height of rectangle obj
         """
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
