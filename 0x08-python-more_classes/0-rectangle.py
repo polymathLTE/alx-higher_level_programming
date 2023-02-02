4f901c7fb07b2660ac24778d8558a487ab983bf7
polymathLTE <lawaltemmanuel@gmail.com>
2023-02-02 04:54:20 +0000

    Added task files 0 - 2

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Lawal Emmanuel
+
+""" This defines a rectangle class """
+class Rectangle:
+    """ simple implementation of a rectangle """
---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# Author - Lawal Emmanuel
+
+""" This defines a rectangle class """
+
+
+class Rectangle:
+    """ simple implementation of a rectangle """
+    def __init__(self, width=0, height=0):
+        """this initializes an object on creation
+        Args:
+            width (int): the init width of rectangle obj
+            height (int): the init height of rectangle obj
+        """
+        self._width = width
+        self._height = height
+
+    @property
+    def width(self):
+        """retrieves the width of the rectangle obj"""
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        """sets the width if the rectangle obj
+        Args:
+            value (int): the setter value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        """retrieves the height of the rectangle obj"""
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        """sets the height if the rectangle obj
+        Args:
+            value (int): the setter value.
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
