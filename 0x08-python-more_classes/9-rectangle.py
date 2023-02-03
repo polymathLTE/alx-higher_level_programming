63f4edcd4981b2287d81d3c89339e4455f58b781
polymathLTE <107139036+polymathLTE@users.noreply.github.com>
2023-02-03 04:46:16 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -15,8 +15,8 @@ class Rectangle:
             width (int): the init width of rectangle obj
             height (int): the init height of rectangle obj
         """
-        self._width = width
-        self._height = height
+        self.width = width
+        self.height = height
         Rectangle.number_of_instances += 1
 
     @property
