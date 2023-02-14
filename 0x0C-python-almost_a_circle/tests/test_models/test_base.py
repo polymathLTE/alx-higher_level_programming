10cf09592cb5a40d9aefbfc3aaf0255d03559d91
polymathLTE <lawaltemmanuel@gmail.com>
2023-02-14 22:50:04 +0000

    Updated test files and conditions

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -15,6 +15,7 @@ import os
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase_instantiation(unittest.TestCase):
