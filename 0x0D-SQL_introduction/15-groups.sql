e59d5863433dbda261b397d816a95d45e0909365
polymathLTE <lawaltemmanuel@gmail.com>
2023-03-17 03:49:25 -0700

    Fixed issue - ordering output

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- counts and lists the number of rows with the same score
-SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score`;
+SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `score` DESC;
