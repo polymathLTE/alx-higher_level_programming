15acc82158f686996f35412b5ae2ad9e8e9e64b4
polymathLTE <lawaltemmanuel@gmail.com>
2023-04-16 11:34:34 +0100

    Updated task files with port and host specs

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,7 @@ username = sys.argv[1]
 password = sys.argv[2]
 dbname = sys.argv[3]
 
-db = MySQLdb.connect(user=username, passwd=password, db=dbname)
+db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbname)
 cur = db.cursor()
 
 cur.execute("SELECT * FROM states")
