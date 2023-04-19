761128ae694cb5d4061390ee968cccd16695f8ea
polymathLTE <lawaltemmanuel@gmail.com>
2023-04-19 18:23:30 +0100

    Added task files and block import-run

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" selects a given name """
+""" lists all cities ordered by their id """
 import MySQLdb
 import sys
-
 
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     dbname = sys.argv[3]
-    search = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost', port=3306, user=username,
                          passwd=password, db=dbname)
     cur = db.cursor()
 
-    cur.execute(f"select * from states where name = '{search}' ORDER BY id")
+    cur.execute("SELECT * FROM cities ORDER BY id")
     st_list = cur.fetchall()
     for i in st_list:
         print(i)
