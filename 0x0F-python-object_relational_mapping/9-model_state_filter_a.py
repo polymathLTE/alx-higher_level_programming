4942166d1992c4bbdf8e50db5aefc388579b1cda
polymathLTE <lawaltemmanuel@gmail.com>
2023-04-20 11:11:12 +0100

    Added task files

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+script that lists all State objects
+from the database hbtn_0e_6_usa
+"""
+from model_state import Base, State
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    # dialect+driver://username:password@host:port/database
+    engine = create_engine(f"mysql+mysqldb://{sys.argv[2]}:{sys.argv[1]}@localhost:3306/{sys.argv[3]}")
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter(State.name.like("%a%")).order_by(State.id):
+        if instance is None:
+            print("Nothing")
+        else:
+            print(instance.id, instance.name, sep=": ")
