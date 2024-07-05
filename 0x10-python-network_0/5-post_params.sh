7841887f752827c830662d58b9caa29fd99e1949
polymathLTE <lawaltemmanuel@gmail.com>
2024-07-05 04:54:24 -0700

    Fixed syntax issue

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends POST request to passed URL and displays response body
-curl -s -d "email=test@gmail.com&subject=I will always be here for PLD -X POST "$1"
+curl -s -d "email=test@gmail.com&subject=I will always be here for PLD" -X POST "$1"
