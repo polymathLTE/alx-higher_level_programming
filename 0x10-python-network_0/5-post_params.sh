1069a57df6ba929ab6da93d25c2490adfd83a159
polymathLTE <lawaltemmanuel@gmail.com>
2024-07-05 04:44:15 -0700

    Updated a POST request value

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends POST request to passed URL and displays response body
-curl -d "email=test@gmail.com&subject=I will always be here for the PLD" -X POST "$1"
+curl -d "email=test@gmail.com&subject=I will always be here for PLD -X POST "$1"
