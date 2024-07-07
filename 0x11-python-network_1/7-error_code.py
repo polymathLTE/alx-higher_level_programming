3cb0b8dae987c759523a6ba97fdacca5bc1722a7
polymathLTE <lawaltemmanuel@gmail.com>
2024-07-07 07:17:25 -0700

    Added documentation to files

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+script that takes in a URL,
+sends a request to the URL and displays the body of the response
+displays Error code if >400
+
+Author - Lawal Emmanuel
+"""
 
 import requests
 import sys
@@ -7,3 +14,5 @@ if __name__ == "__main__":
     response = requests.get(sys.argv[1])
     if response.status_code >= 400:
         print(f"Error code: {response.status_code}")
+    else:
+        print(response.text)
